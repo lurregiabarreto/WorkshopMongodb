45b23c9ddc4a4d11c931244d4f4ba865ab7c3e1a
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-08 17:18:30 -0300

    Connecting to MongoDB with Repository and Service

---
+++ WorkshopMongo/src/main/java/com/lurregiabarreto/WorkshopMongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.lurregiabarreto.WorkshopMongo.services;
+
+import com.lurregiabarreto.WorkshopMongo.domain.User;
+import com.lurregiabarreto.WorkshopMongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
