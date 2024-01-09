f423703ceb924779e654d670f1f0340e90b16865
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-09 16:05:41 -0300

    Adding comments to posts

--- WorkshopMongo/src/main/java/com/lurregiabarreto/WorkshopMongo/domain/Post.java
+++ WorkshopMongo/src/main/java/com/lurregiabarreto/WorkshopMongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.lurregiabarreto.WorkshopMongo.domain;
 
 import com.lurregiabarreto.WorkshopMongo.dto.AuthorDTO;
+import com.lurregiabarreto.WorkshopMongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -68,6 +72,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
