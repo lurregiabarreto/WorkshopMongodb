6d002e3dfa2fb0bfbb84579433b84d01d8c67f94
Lurrégia Barreto Adão <luhhbarreto@gmail.com>
2024-01-09 15:51:04 -0300

    Endpoint to return a user's posts

--- WorkshopMongo/src/main/java/com/lurregiabarreto/WorkshopMongo/resources/UserResource.java
+++ WorkshopMongo/src/main/java/com/lurregiabarreto/WorkshopMongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lurregiabarreto.WorkshopMongo.resources;
 
+import com.lurregiabarreto.WorkshopMongo.domain.Post;
 import com.lurregiabarreto.WorkshopMongo.domain.User;
 import com.lurregiabarreto.WorkshopMongo.dto.UserDTO;
 import com.lurregiabarreto.WorkshopMongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
